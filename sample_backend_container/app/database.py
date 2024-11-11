f62ce89bfe1a2d44ed7fcdb1e5b2df6217fc655b
dbd-fish <ryo.kokai.g@gmail.com>
2024-11-11 22:10:30 +0900

    FastAPI非推奨の処理を変更。また、main.pyが動かなくっていたため修正。

--- sample_backend_container/app/database.py
+++ sample_backend_container/app/database.py
@@ -1,4 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base  #
+import configparser
+from databases import Database
+
+# alembic.iniファイルからDB接続
+config = configparser.ConfigParser()
+config.read('alembic.ini')  # alembic.iniのパスは適宜調整
+DATABASE_URL = config.get('alembic', 'sqlalchemy.url')
+# Databasesパッケージの設定
+database = Database(DATABASE_URL)
 
 # Baseクラスの定義
 Base = declarative_base()
--- sample_backend_container/main.py
+++ sample_backend_container/main.py
@@ -1,31 +1,23 @@
-from fastapi import FastAPI  # FastAPIフレームワークをインポート
-from app.database import database  # データベース接続オブジェクトをインポート
-from app.routes import router  # アプリケーションのルーティングをインポート
+from fastapi import FastAPI
+from app.database import database
+from app.routes import router
+from contextlib import asynccontextmanager
 
-# FastAPIアプリケーションインスタンスを作成
-app = FastAPI()
-
-# アプリケーションが起動する際に呼び出されるイベントハンドラ
-@app.on_event("startup")
-async def startup():
-    # データベースに接続
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # アプリケーション起動時の処理
     await database.connect()
-
-# アプリケーションがシャットダウンする際に呼び出されるイベントハンドラ
-@app.on_event("shutdown")
-async def shutdown():
-    # データベース接続を切断
+    yield
+    # アプリケーション終了時の処理
     await database.disconnect()
+
+# FastAPIアプリケーションインスタンスを作成し、lifespanを設定
+app = FastAPI(lifespan=lifespan)
 
 # インポートしたルーターをアプリケーションに追加
 app.include_router(router)
 
 # このスクリプトが直接実行された場合、Uvicornサーバーを起動
 if __name__ == "__main__":
-    import uvicorn  # Uvicornサーバーをインポート
-    # UvicornでFastAPIアプリケーションを起動
+    import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-    # "main:app" は "main.py" の "app" インスタンスを指定している
-    # host="0.0.0.0" は全てのネットワークインターフェイスでリッスン
-    # port=8000 は8000番ポートで起動
-    # reload=True はコード変更時に自動で再起動するように設定（開発モード用）
