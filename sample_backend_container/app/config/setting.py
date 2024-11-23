4c61c44d7d402107e49c37bc0bcc2248f7eb6d83
dbd-fish <ryo.kokai.g@gmail.com>
2024-11-23 13:33:58 +0900

    不要な定数を削除

--- sample_backend_container/app/config/setting.py
+++ sample_backend_container/app/config/setting.py
@@ -6,8 +6,7 @@ from pydantic_settings import BaseSettings
 class Setting(BaseSettings):
     # アプリケーション基本設定
     APP_NAME: str = "Sample FastAPI App"
-    VERSION: str = "1.0.0"
-    DEBUG: bool = True
+
 
     # セキュリティ設定
     SECRET_KEY: str = "your_secret_key_here"  # JWT署名用の秘密鍵
@@ -18,15 +17,9 @@ class Setting(BaseSettings):
     SQL_LOG_DIRECTORY: str  = "logs/sql"
 
     # データベース設定
-    # DATABASE_URL: str = "postgresql+asyncpg://user:password@localhost:5432/sample_db"
+    # alembic.iniに記載
 
-    # メール関連の設定（例: パスワードリセットで使用する場合）
-    SMTP_SERVER: str = "smtp.example.com"
-    SMTP_PORT: int = 587
-    SMTP_USERNAME: str = "your_email@example.com"
-    SMTP_PASSWORD: str = "your_password"
-    EMAIL_FROM: str = "your_email@example.com"
-    EMAIL_FROM_NAME: str = "Sample App"
+
 
     class Config:
         env_file = ".env"  # .envファイルから環境変数を読み込む設定
