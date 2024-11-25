92bb21854234243057ea4079f0ffdb72f7d10651
dbd-fish <ryo.kokai.g@gmail.com>
2024-11-25 10:34:46 +0900

    レポート用のルーター定義を修正

--- sample_backend_container/app/routes.py
+++ sample_backend_container/app/routes.py
@@ -5,7 +5,7 @@ from app.controllers.auth_controller import router as auth_router
 router = APIRouter()
 
 # レポート用のルーター定義  
-router.include_router(report_router, prefix="")
+router.include_router(report_router, prefix="/report",  tags=["report"])
 
 # 認証用のルーター
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
